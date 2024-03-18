6f54a1b8cc2777ad5a5682d83535a246d5abc8fb
krishan <krishan5335@gmail.com>
2024-03-18 11:23:55 +0530

    Initial Commit

--- playwright.config.js
+++ playwright.config.js
@@ -34,7 +34,7 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     //trace: 'on',
-    //trace: 'retain-on-failure',
+    trace: 'retain-on-failure',
     storageState:"./LoginAuth.json",
     screenshot: "only-on-failure",
     video:"retain-on-failure",
