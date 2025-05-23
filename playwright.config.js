3d2cee3cb07c331b08e034cfdad4ddcbdf2f76cc
Sadith QA <sadith@smashtaps.com>
2025-05-23 22:21:51 +1000

    Remove Playwright_Molly to add as submodule

--- playwright.config.js
+++ playwright.config.js
@@ -18,8 +18,8 @@ module.exports = defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  //retries: process.env.CI ? 2 : 0,
-  retries:0,
+  retries: process.env.CI ? 2 : 0,
+  //retries:0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -40,6 +40,7 @@ module.exports = defineConfig({
     //video:"retain-on-failure",
     //video:"on",
     actionTimeout: 120000,
+    headless: true,                    // Force headless mode everywhere (especially CI)
     
     
     
